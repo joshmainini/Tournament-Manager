ca14752cdf64326b8d6d2685076995806e62a65e
Josh Mainini <joshmainini@outlook.com>
2017-08-02 09:17:54 -0400

    Working on Selenium Tests

---
+++ Capstone.Tests/SeleniumTests/AccountTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebApplication1.Tests.PageObjects;
+
+namespace WebApplication1.Tests.SeleniumTests
+{
+	[TestClass]
+	public class AccountTests
+	{
+		private static IWebDriver driver;
+
+		[ClassInitialize]
+		public static void SetUp(TestContext context)
+		{
+			driver = new ChromeDriver();
+			driver.Navigate().GoToUrl("http://localhost:55393/");
+		}
+
+		[ClassCleanup]
+		public static void Cleanup()
+		{
+			driver.Close();
+			driver.Quit();
+		}
+
+		[TestMethod]
+		public void Register_GoToHome()
+		{
+			Register entryPage = new Register(driver);
+			entryPage.Navigate();
+
+			entryPage.FillOutForm("jellymo@jellymo.com", "Jellymo2", "Jellymo2");
+
+
+		}
+	}
+}
