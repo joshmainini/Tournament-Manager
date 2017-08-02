b2898fda6920244d363c3ebf61adc32de2115647
Josh Mainini <joshmainini@outlook.com>
2017-08-02 15:24:21 -0400

    Completed selenium tests for sprint 1

--- Capstone.Tests/SeleniumTests/AccountTests.cs
+++ Capstone.Tests/SeleniumTests/AccountTests.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using WebApplication1.Tests.PageObjects;
+using WebApplication1.DAL;
 
 namespace WebApplication1.Tests.SeleniumTests
 {
 	[TestClass]
 	public class AccountTests
 	{
+		UserDAL dal = new UserDAL(@"Data Source=localhost\sqlexpress;Initial Catalog=Slime;Integrated Security=True");
 		private static IWebDriver driver;
 
+		
 		[ClassInitialize]
 		public static void SetUp(TestContext context)
 		{
@@ -23,7 +27,7 @@ namespace WebApplication1.Tests.SeleniumTests
 		}
 
 		[ClassCleanup]
-		public static void Cleanup()
+		public static void CleanupDriver()
 		{
 			driver.Close();
 			driver.Quit();
@@ -49,6 +53,7 @@ namespace WebApplication1.Tests.SeleniumTests
 			LoginResult result = entryPage.FillOutForm("jellymo@jellymo.com", "Jellymo2@");
 
 			Assert.AreEqual("Hello jellymo@jellymo.com!", result.Success.Text);
+			dal.DeleteUser("jellymo@jellymo.com");
 		}
 	}
 }
