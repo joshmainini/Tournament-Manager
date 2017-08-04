cc4a373fdec896ac9c2ba034b2e021e782c21f32
Josh Mainini <joshmainini@outlook.com>
2017-08-04 12:58:20 -0400

    Adding tournament and player controllers

--- Capstone/Models/TournamentManagerContext.cs
+++ Capstone/Models/TournamentManagerContext.cs
@@ -8,6 +8,10 @@ namespace WebApplication1.Models
 {
 	public class TournamentManagerContext: DbContext
 	{
+		public TournamentManagerContext() 
+			: base("DefaultConnection")
+		{
+		}
 		public DbSet<Tournament> Tournaments { get; set; }
 		public DbSet<Match> Matches { get; set; }
 		public DbSet<Team> Teams { get; set; }
