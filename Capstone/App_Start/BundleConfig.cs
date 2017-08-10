f10556f282e777588a321fd111fa33fd070105c4
Spencer Lucas <jspencerlucas@gmail.com>
2017-08-10 10:05:03 -0400

    Added banner and minor css change to bracket view

--- Capstone/App_Start/BundleConfig.cs
+++ Capstone/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace WebApplication1
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
